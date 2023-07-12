d04fe51d2715654204c0827f50704c79644052e6
Adam J. Butler <85082674+adam248@users.noreply.github.com>
2023-07-12 10:08:15 +0930

    +auto numlock

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -10,7 +10,7 @@
 # Personal Programs: 222
 # System-Wide Programs: 300
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
   user = "adam"; # currently unused but if you want to use this variable do this: ${user}
@@ -114,6 +114,17 @@ in
     };
   };
 
+  systemd.services.numLockOnTty = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      # /run/current-system/sw/bin/setleds -D +num < "$tty";
+      ExecStart = lib.mkForce (pkgs.writeShellScript "numLockOnTty" ''
+        for tty in /dev/tty{1..6}; do
+            ${pkgs.kbd}/bin/setleds -D +num < "$tty";
+        done
+      '');
+      };
+  };
 
   # Configure keymap in X11
   # services.xserver.layout = "us";
