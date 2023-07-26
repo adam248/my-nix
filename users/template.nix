c41471a5b36c4d9502894e34417a06f669b3c6bb
Adam J. Butler <85082674+adam248@users.noreply.github.com>
2023-07-26 16:18:38 +0930

    basic user template with home-manager

--- users/template.nix
+++ users/template.nix
@@ -2,3 +2,26 @@
 # PLEASE! make a copy before EDITING!
 # make sure to add to default.nix imports = [];
 # when finished editing
+
+let
+    user-name = "joe";
+
+in {
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.${user-name} = {
+        isNormalUser = true;
+        extraGroups = [
+            "wheel" # wheel enables sudo for this user 
+        ];
+        initialPassword = user-name;
+        packages = with pkgs; [
+
+        ];
+    };
+
+    home-manager.users.${user-name} = { pkgs, ... }: {
+    home.stateVersion = "23.05";
+    home.packages = with pkgs; [
+        typst
+    ];
+}
