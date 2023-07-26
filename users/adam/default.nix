65185a1cca94860cb75fc2f3d630548c3bcaf13e
Adam J. Butler <85082674+adam248@users.noreply.github.com>
2023-07-26 16:11:42 +0930

    setup user template files

---
+++ users/adam/default.nix
@@ -0,0 +1,2 @@
+# this file is what is imported when you add
+# ./users/adam to a config file's imports = []; 
---
+++ users/template.nix
@@ -0,0 +1,4 @@
+# this is a template user config
+# PLEASE! make a copy before EDITING!
+# make sure to add to default.nix imports = [];
+# when finished editing
