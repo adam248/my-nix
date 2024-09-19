f24246d4bb38f8d46ee2ea72842c9f3970f89e0e
Adam J. Butler <85082674+adam248@users.noreply.github.com>
2024-09-19 11:13:09 +0800

    %set nameservers and use ethx interface name format

--- isos/base-image/iso.nix
+++ isos/base-image/iso.nix
@@ -36,15 +36,15 @@
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDib7NjQONFsnHSFqGzd1ozleQz3vsJfm9G7l69SHKoXE6jaKYGuz6sL4LLBGsRAT5KjFi5JNznVNKirf7iAwI8Qra+IAnoPwCtZ5ftVs5sv9g+unccZh+R3FyS1fbGfSlqAmFn9v0UNP4mJXEhX2GM4/hw/8Unp0b9fFMDvf56ldvvCENOy6dZVXhsSbDV423DLsKv0GVousebdW+f4Z0vqKC6H6/T/AJRB7WS/NLeXNW+762nOs/aPkEZ17E7Pe1N1H3WZu3jZivIcXfnNsVpu68133aFX8eZPiMRtbeVBh3T1nbeCrdkSE9oi9m9/Q6DahEtG6N/sZha8RLfYaP4srX/9xhMa/iZiexQ/4INFrUxH8DmKjmr1FF8lpsMcIAg6sjBGpx0rjWJ5HslrIxircl7k8rlk6AtPVPmWj9/vdHL2vq9/Tr+kT9LVHw7L0piGoZkI0K7z++ao2TlOBV8M8Jm19NpBBrL12CeK63QySuNGsCb5XCvtasCNRJIkdBa1u7P3qd7XxEakzaO5OAwnzxptNTuYIiQ33BDEzrfOSEs3jJ71EfrMO1bdTnCj2eDsttuUtdV01cIe1M8W85FetaxHJiGdWTD9ngvQPrs9t1Hf0Hipt1n4hZZEpZN0uDqfLJcVOR62cmz7WHMndfmuk+0ko2aZW5IDbW8JYMItQ== adam@nxbx-dsktp"
   ];
 
-#  networking = {
-#    usePredictableInterfaceNames = false;
+  networking = {
+    usePredictableInterfaceNames = false;
 #    interfaces.eth0.ipv4.addresses = [{
 #        address = "192.168.0.10";
 #        prefixLength = 24;
 #    }];
 #    defaultGateway = "192.168.0.1";
-#    nameservers = [ "1.1.1.1" "8.8.8.8" ];
-#  };
+    nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  };
 
 # TODO: add wireguard vpn autoconnection setup (need server first)
 #       this will allow for easy network-based installation (no more finding a monitor & keyboard)
